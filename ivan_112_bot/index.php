fded88840805d06c78ac6ca8597444c0623db9be
ivan-andriankin <andriankin.ivan@yandex.ru>
2022-07-23 20:53:22 +0600

    actualize project

--- ivan_112_bot/index.php
+++ ivan_112_bot/index.php
@@ -25,14 +25,65 @@ $message = mb_strtolower(($data['text'] ? $data['text'] : $data['data']),'utf-8'
 switch ($message)
 {
     case 'текст':
-
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Here is my answer'
+        ];
         break;
     
     case 'кнопки':
-        
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Here are my buttons',
+            'reply_markup' => [
+                'resize_keyboard' => true,
+                'keyboard' => [
+                    [
+                        ['text' => 'Button 1'],
+                        ['text' => 'Button 2'],
+                    ],
+                    [
+                        ['text' => 'Button 3'],
+                        ['text' => 'Button 4'],
+                    ]
+                ]
+            ]
+        ];
         break;
 
     case 'видео':
-
+        $method = 'sendMessage';
+        $send_data = [
+            'text' => 'Here is my video'."\n".'https://www.youtube.com/watch?v=mwVVE1tVHls&list=PL7nS_iZvABcPpfnvDhFlMD0B5NNknyNE-&index=94&t=173s'
+            
+        ];
         break;
+
+    default:
+        $send_data = [
+            'method' => 'sendMessage',
+            'text' => 'Possible commands:'."\n".'текст'."\n".'кнопки'."\n".'видео'
+        ];
+}
+
+#Add user's data
+$send_data['chat_id'] = $data['chat']['id'];
+
+$res = sendTelegram($method, $send_data);
+
+function sendTelegram($method, $data, $headers = [])
+{
+    $curl = curl_init();
+    curl_setopt_array($curl, [
+        CURLOPT_POST => 1,
+        CURLOPT_HEADER => 0,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_URL => 'https://api.telegram.org/bot' . TOKEN . '/' . $method,
+        CURLOPT_POSTFIELDS => json_encode($data),
+        CURLOPT_HTTPHEADER => array_merge(array("Content-Type: application/json"), $headers)
+    ]);
+
+    $result = curl_exec($curl);
+    curl_close($curl);
+    return (json_decode($result, 1) ? json_decode($result, 1) : $result);
 }
